8ddf404bc411dbb05a37bdfe37596c77a07ab581
pawelbrzoza <brzozik95@gmail.com>
2017-04-19 21:52:38 +0200

    Add project files.

---
+++ CPM/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace CPM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.TextBox();
+            this.duration = new System.Windows.Forms.TextBox();
+            this.begin = new System.Windows.Forms.TextBox();
+            this.end = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.enterData = new System.Windows.Forms.Button();
+            this.findCMP = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(132, 66);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 20);
+            this.name.TabIndex = 0;
+            this.name.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // duration
+            // 
+            this.duration.Location = new System.Drawing.Point(132, 102);
+            this.duration.Name = "duration";
+            this.duration.Size = new System.Drawing.Size(100, 20);
+            this.duration.TabIndex = 1;
+            // 
+            // begin
+            // 
+            this.begin.Location = new System.Drawing.Point(132, 139);
+            this.begin.Name = "begin";
+            this.begin.Size = new System.Drawing.Size(100, 20);
+            this.begin.TabIndex = 2;
+            // 
+            // end
+            // 
+            this.end.Location = new System.Drawing.Point(132, 180);
+            this.end.Name = "end";
+            this.end.Size = new System.Drawing.Size(100, 20);
+            this.end.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(331, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(149, 134);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Czynność:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nazwa:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Czas trwania: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Początek:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(57, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Koniec:";
+            // 
+            // enterData
+            // 
+            this.enterData.Location = new System.Drawing.Point(145, 219);
+            this.enterData.Name = "enterData";
+            this.enterData.Size = new System.Drawing.Size(75, 23);
+            this.enterData.TabIndex = 10;
+            this.enterData.Text = "Wprowadź";
+            this.enterData.UseVisualStyleBackColor = true;
+            this.enterData.Click += new System.EventHandler(this.enterData_Click);
+            // 
+            // findCMP
+            // 
+            this.findCMP.Location = new System.Drawing.Point(369, 219);
+            this.findCMP.Name = "findCMP";
+            this.findCMP.Size = new System.Drawing.Size(75, 23);
+            this.findCMP.TabIndex = 11;
+            this.findCMP.Text = "Znajdź CPM";
+            this.findCMP.UseVisualStyleBackColor = true;
+            this.findCMP.Click += new System.EventHandler(this.findCMP_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(61, 271);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Ścieżka krytyczna:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(163, 271);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 13;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(170, 271);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(16, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "...";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.4127F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.5873F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(554, 59);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.tableLayoutPanel1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(787, 327);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.findCMP);
+            this.Controls.Add(this.enterData);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.end);
+            this.Controls.Add(this.begin);
+            this.Controls.Add(this.duration);
+            this.Controls.Add(this.name);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox duration;
+        private System.Windows.Forms.TextBox begin;
+        private System.Windows.Forms.TextBox end;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button enterData;
+        private System.Windows.Forms.Button findCMP;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ CPM/Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPM
+{
+   
+    public partial class Form1: Form
+    {
+        private class Event
+        {
+            public int EarliestTime { get; set; }
+
+            public int LatestTime { get; set; }
+
+            public int SpareTime { get; set; }
+        }
+
+        private class Activity
+        {
+
+            public string Name { get; set; }
+            public int Duration { get; set; }
+            public int Begin { get; set; }
+            public int End { get; set; }
+
+            public Event First { get; private set; }
+            public Event Next { get; private set; }
+
+            public Activity()
+            {
+                First = new Event();
+                Next = new Event();
+            }
+        }
+
+      
+        List<Activity> listActivity;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listActivity = new List<Activity>();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void enterData_Click(object sender, EventArgs e)
+        {
+            
+            listBox1.Items.Add(name.Text + ": " + duration.Text + "    " + begin.Text + "    -    " + end.Text);
+
+            listActivity.Add(new Activity() { Name = name.Text, Duration = Convert.ToInt16(duration.Text), Begin = Convert.ToInt16(begin.Text), End = Convert.ToInt16(end.Text)});
+
+        }
+
+        private string tab ="";
+
+        private int maxDuration(int val)
+        {
+            var max = 0;
+
+            for ( var i = 0; i < listActivity.Count; i++ )
+            {
+                if ( listActivity[i].End == val)
+                {
+                    var sum = listActivity[i].First.EarliestTime + listActivity[i].Duration;
+                    if (sum > max)
+                    {
+                        max = sum;
+                        
+                    }
+                }
+            }
+            return max;
+        }
+
+        private int maxPath(int val)
+        {
+            var max = 0;
+            var maxID = 0;
+
+            for( var i = 0; i < listActivity.Count; i++ )
+            {
+                if( listActivity[i].End == val )
+                {
+                    var sum = listActivity[i].First.LatestTime + listActivity[i].Duration;
+                    if (sum > max)
+                    {
+                        max = sum;
+                        maxID = i;
+                    }
+                }
+            }
+            return maxID;
+        }
+
+        private void updateLatestTime(int val, int dur)
+        {
+            for (int i = 0; i < listActivity.Count; i++)
+            {
+                if (listActivity[i].Begin == val)
+                {
+                    listActivity[i].First.LatestTime = dur;
+                }
+            }
+        }
+
+        private void findCMP_Click(object sender, EventArgs e)
+        {
+            // ZAD 1
+            listActivity.Add(new Activity() {
+                Name = "A",
+                Duration = 3,
+                Begin = 1,
+                End = 2
+            });
+            listActivity.Add(new Activity() {
+                Name = "B",
+                Duration = 4,
+                Begin = 2,
+                End = 3
+            });
+            listActivity.Add(new Activity() {
+                Name = "C",
+                Duration = 6,
+                Begin = 2,
+                End = 4
+            });
+            listActivity.Add(new Activity() {
+                Name = "D",
+                Duration = 7,
+                Begin = 3,
+                End = 5
+            });
+            listActivity.Add(new Activity() {
+                Name = "E",
+                Duration = 1,
+                Begin = 5,
+                End = 7
+            });
+            listActivity.Add(new Activity() {
+                Name = "F",
+                Duration = 2,
+                //Duration = 4,
+                Begin = 4,
+                End = 7
+            });
+            listActivity.Add(new Activity() {
+                Name = "G",
+                Duration = 3,
+                Begin = 4,
+                End = 6
+            });
+            listActivity.Add(new Activity() {
+                Name = "H",
+                //Duration = 2,
+                Duration = 4,
+                Begin = 6,
+                End = 7
+            });
+            listActivity.Add(new Activity() {
+                Name = "I",
+                Duration = 1,
+                Begin = 7,
+                End = 8
+            });
+            listActivity.Add(new Activity() {
+                Name = "J",
+                Duration = 2,
+                Begin = 8,
+                End = 9
+            });
+
+            // ZAD 2
+            //listActivity.Add(new Activity() {
+            //    Name = "A",
+            //    Duration = 6,
+            //    Begin = 1,
+            //    End = 2
+            //});
+            //listActivity.Add(new Activity() {
+            //    Name = "B",
+            //    Duration = 10,
+            //    Begin = 1,
+            //    End = 3
+            //});
+            //listActivity.Add(new Activity() {
+            //    Name = "C",
+            //    Duration = 6,
+            //    Begin = 2,
+            //    End = 3
+            //});
+            //listActivity.Add(new Activity() {
+            //    Name = "D",
+            //    Duration = 12,
+            //    Begin = 2,
+            //    End = 5
+            //});
+            //listActivity.Add(new Activity() {
+            //    Name = "E",
+            //    Duration = 5,
+            //    Begin = 3,
+            //    End = 4
+            //});
+            //listActivity.Add(new Activity() {
+            //    Name = "F",
+            //    Duration = 8,
+            //    //Duration = 4,
+            //    Begin = 3,
+            //    End = 5
+            //});
+            //listActivity.Add(new Activity() {
+            //    Name = "G",
+            //    Duration = 8,
+            //    Begin = 4,
+            //    End = 6
+            //});
+            //listActivity.Add(new Activity() {
+            //    Name = "H",
+            //    //Duration = 2,
+            //    Duration = 7,
+            //    Begin = 5,
+            //    End = 6
+            //});
+            //listActivity.Add(new Activity() {
+            //    Name = "I",
+            //    Duration = 8,
+            //    Begin = 5,
+            //    End = 7
+            //});
+            //listActivity.Add(new Activity() {
+            //    Name = "J",
+            //    Duration = 6,
+            //    Begin = 6,
+            //    End = 7
+            //});
+            //listActivity.Add(new Activity() {
+            //    Name = "K",
+            //    Duration = 7,
+            //    Begin = 7,
+            //    End = 8
+            //});
+
+            listActivity[0].First.EarliestTime = 0;
+            listActivity[0].First.LatestTime = 0;
+            listActivity[0].First.SpareTime = 0;
+            //listActivity[0].Next.EarliestTime = listActivity[0].Duration;
+
+            // WERSJA BEZ WYSZUKIWANIA NAJDLUZSZEGO CZASU
+            //for (int i = 0; i<listActivity.Count; i++)
+            //{
+            //    for (int j = i + 1; j < listActivity.Count; j++)
+            //    {
+            //        if (listActivity[i].End == listActivity[j].Begin)
+            //        {
+            //            listActivity[j].First.EarliestTime = listActivity[i].Duration + listActivity[i].First.EarliestTime;
+            //        }
+            //    }
+            //}
+
+            // WERSJA Z WYSZUKIWANIEM NAJDLUZSZEGO CZASU (IMPROVED)
+
+            int j;
+            for( int i = 0; i < listActivity.Count; i++ )
+            {
+                for(j = i + 1; j < listActivity.Count; j++ )
+                {
+                    if( listActivity[i].End == listActivity[j].Begin )
+                    {
+                        listActivity[j].First.EarliestTime = listActivity[i].Duration + listActivity[i].First.EarliestTime;
+
+                        var maxDur = maxDuration(listActivity[j].Begin);
+                        if( listActivity[j].First.EarliestTime != maxDur )
+                        {
+                            listActivity[j].First.EarliestTime = maxDur;
+                            
+                        }
+                    }
+                }
+            }
+            // SPRAWDZANIE DLA OSTATNIEJ CZYNNOSCI W NEXT ZDARZENIU
+            j = listActivity.Count - 1;
+            
+            listActivity[j].Next.LatestTime = listActivity[j].Next.EarliestTime = maxDuration(listActivity[j].End);
+
+            // OBLICZANIE NAJPOZNIJESZYCH MOŻLIWYCH CZASÓW ZAJŚCIA ZDARZENIA
+
+            // DLA PIERWSZEJ CZYNNOSCI OD KONCA:
+
+            listActivity[j].First.LatestTime = listActivity[j].Next.LatestTime - listActivity[j].Duration;
+
+            // DLA POZOSTALYCH
+            int sub;
+            for( int i = 1; i < listActivity.Count - 1; i++ )
+            {
+                listActivity[i].First.LatestTime = 999999;
+            }
+
+            for( int i = listActivity.Count-1; i > 0; i-- )
+            {
+                for( j = 0; j<listActivity.Count; j++ )
+                {
+                    if (listActivity[j].End == listActivity[i].Begin)
+                    {
+                        sub = listActivity[i].First.LatestTime - listActivity[j].Duration;
+                        if (listActivity[j].First.LatestTime > sub)
+                        {
+                            listActivity[j].First.LatestTime = sub;
+                            updateLatestTime(listActivity[j].Begin, sub);
+                        }
+                    }
+                   
+                }
+            }
+
+            // UZUPELNIENIE ZDARZEN
+            for (int i = 0; i < listActivity.Count; i++)
+            {
+                for (int k = i+1; k < listActivity.Count; k++)
+                {
+                    if (listActivity[i].End == listActivity[k].Begin)
+                    {
+                        listActivity[i].Next.EarliestTime = listActivity[k].First.EarliestTime;
+                        listActivity[i].Next.LatestTime = listActivity[k].First.LatestTime;
+                        listActivity[i].Next.SpareTime = listActivity[k].First.SpareTime;
+                    }
+                }
+            }
+
+            // UZUPEŁNIANIE/OBLICZENIE ZAPASOWEGO CZASU
+            for( int i = 0; i < listActivity.Count; i++ )
+            {
+                listActivity[i].First.SpareTime = listActivity[i].First.LatestTime - listActivity[i].First.EarliestTime;
+                listActivity[i].Next.SpareTime = listActivity[i].Next.LatestTime - listActivity[i].Next.EarliestTime;
+            }
+
+            foreach (var activ in listActivity)
+            {
+                Console.WriteLine("Nazwa czynnosci: " + activ.Name);
+                Console.WriteLine("Czas trwania: " + activ.Duration + "\n");
+                Console.WriteLine("Poczatek: " + activ.Begin);
+                Console.WriteLine("Najwczesniejszy czas trwania: " + activ.First.EarliestTime);
+                Console.WriteLine("Najpozniejszyniejszy czas trwania: " + activ.First.LatestTime);
+                Console.WriteLine("Zapas czasu: " + activ.First.SpareTime + "\n");
+                Console.WriteLine("Koniec: " + activ.End);
+                Console.WriteLine("Najwczesniejszy czas trwania: " + activ.Next.EarliestTime);
+                Console.WriteLine("Najpozniejszyniejszy czas trwania: " + activ.Next.LatestTime);
+                Console.WriteLine("Zapas czasu: " + activ.Next.SpareTime + "\n\n");
+            }
+
+            // WYBIERANIE CZYNNOSCI NA SCIEZKE KRYTYCZNA
+            
+            int temp = 0;
+            int temp2 = 0;
+            for( int i = 0; i < listActivity.Count; i++ )
+            {
+                temp += listActivity[i].Duration;
+                if ((listActivity[i].First.SpareTime + listActivity[i].Next.SpareTime) == 0 &&
+                    (temp - listActivity[i].Next.EarliestTime) == 0)
+                {
+                    tab += listActivity[i].Name + " ";
+                    temp2 = temp;
+                }
+                else
+                {
+                    temp = temp2;
+                }
+                
+            }
+            label10.Text = tab;
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
